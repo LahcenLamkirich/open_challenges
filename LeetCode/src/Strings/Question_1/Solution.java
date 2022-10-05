104bbfd0a55247b3438f54f7ae6ea9672096149f
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-10-05 01:14:49 +0100

    new commit

--- LeetCode/src/Strings/Question_1/Solution.java
+++ LeetCode/src/Strings/Question_1/Solution.java
@@ -1,4 +1,12 @@
 package Strings.Question_1;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("ab##", "lahcen"));
+    }
+
+    public static boolean backspaceCompare(String s, String t) {
+        return true ;
+    }
 }
