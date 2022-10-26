b9b9fba4667d6dee9f000c7b93c07bcdbccab228
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-10-27 00:49:27 +0100

    new commit

--- LeetCode/src/Arrays/add_two_numbers/Solution.java
+++ LeetCode/src/Arrays/add_two_numbers/Solution.java
@@ -1,5 +1,30 @@
 package Arrays.add_two_numbers;
 
+import java.util.LinkedList;
+
 public class Solution {
-    
+    public static void main(String[] args) {
+        LinkedList<Integer> firstLinkedList = new LinkedList<>();
+        LinkedList<Integer> secondLinkedList = new LinkedList<>();
+
+        firstLinkedList.add(2);
+        firstLinkedList.add(4);
+        firstLinkedList.add(3);
+
+        secondLinkedList.add(5);
+        secondLinkedList.add(6);
+        secondLinkedList.add(4);
+
+        // Reverse the first One :
+        firstLinkedList.forEach(number -> {
+            System.out.println(number);
+        });
+        // Reverse the second One :
+        secondLinkedList.forEach(number -> {
+            System.out.println(number);
+        });
+
+        // And then reverse the result :         
+
+    }
 }
