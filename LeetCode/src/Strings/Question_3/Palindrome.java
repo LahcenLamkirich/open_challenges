0f3883aca8e594ca28056f981013689fb47f3d48
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-11-01 00:57:16 +0100

    new commit

--- LeetCode/src/Strings/Question_3/Palindrome.java
+++ LeetCode/src/Strings/Question_3/Palindrome.java
@@ -1,20 +1,75 @@
 package Strings.Question_3;
 
 public class Palindrome {
+
+    public static String newStr ;
+
     public static boolean isPalindrome(String str) {
         String reversedStr = new String();
+        str = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
-        System.out.println("The length : " + str.length());
+
         for(int i = str.length() - 1 ; i >= 0 ; i--){
             reversedStr += Character.toString(str.charAt(i));
         }
 
         if(str.equals(reversedStr))
             return true;
+
         return false;
     }
 
+    public static Boolean isPalindromeII(String str) {
+        String newStr ;
+        if (isPalindrome(str))
+            return true;
+        else {
+            for (int i = 0; i < str.length(); i++) {
+                newStr = str.replace(String.valueOf(str.charAt(i)), "");
+                if (isPalindrome(newStr))
+                    return true;
+            }
+        }
+
+        return false ;
+ }
+    // This one is recursive :
+    public static Boolean isPalindromeIII(String str) {
+
+        int left = 0 ;
+        int right = str.length() - 1 ;
+
+        while(left <= right){
+            if(str.charAt(left) != str.charAt(right)){
+                return isValid(str, left+1, right) || isValid(str, left , right-1);
+            }
+
+            left++ ;
+            right--;
+        }
+        return true ;
+    }
+
+    public static Boolean isValid(String str, int i , int j ) {
+
+        int left = i ;
+        int right = j;
+
+        while(left <= right){
+
+            if(str.charAt(left) != str.charAt(right)){
+                return false ;
+            }
+
+            left++ ;
+            right--;
+        }
+
+        return true ;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome("aba"));
+        System.out.println(isPalindromeII("racecar"));
+        System.out.println(isPalindromeIII("abca"));
     }
 }
